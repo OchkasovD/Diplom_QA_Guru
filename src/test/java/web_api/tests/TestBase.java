4aed3ea4da5d57151df0edeba2474650c43768eb
d.ochkasov <d.ochkasov@qiwi.com>
2023-07-13 16:33:07 +0300

    add telegram notifications

--- src/test/java/web_api/tests/TestBase.java
+++ src/test/java/web_api/tests/TestBase.java
@@ -14,6 +14,8 @@ import web_api.pages.AuthorizationPage;
 import web_api.pages.MainPage;
 import web_api.pages.ProductPage;
 
+import java.util.Map;
+
 import static java.lang.String.format;
 
 
@@ -29,19 +31,27 @@ public class TestBase {
     static void beforeUrl() {
         RestAssured.baseURI = System.getProperty("baseURL", "https://shop1.emagazin.info");
         Configuration.baseUrl = System.getProperty("baseURL", "https://shop1.emagazin.info");
-        Configuration.browser = System.getProperty("browser", "chrome");
+        String[] browserAndVersion = System.getProperty("browser", "chrome:100.0").split(":");
+        Configuration.browser = browserAndVersion[0];
+        Configuration.browserVersion = browserAndVersion[1];
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100");
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.pageLoadStrategy = "eager";
+
+        Configuration.remote = String.format("https://%s:%s@%s",
+                System.getProperty("selenoidLogin", "user1"),
+                System.getProperty("selenoidPassword", "1234"),
+                System.getProperty("selenoidUrl", "https://selenoid.autotests.cloud/wd/hub").replace("https://", "")
+        );
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.remote = format("https://%s:%s@%s", config.login(), config.password(), config.remoteUrl());
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
 
-
-
+        Configuration.browserCapabilities = capabilities;
     }
+
 
     @AfterEach
     void addAttachments() {
