202525faf8055b21203c9000d7ffe65918a04fc4
d.ochkasov <d.ochkasov@qiwi.com>
2023-07-17 11:08:59 +0300

    add telegram notifications fix api test

--- src/test/java/web_api/specs/Specifications.java
+++ src/test/java/web_api/specs/Specifications.java
@@ -14,6 +14,7 @@ public class Specifications {
     public static RequestSpecification requestSpec = with()
             .log().uri()
             .log().all()
+            .filter(withCustomTemplates())
             .contentType("application/x-www-form-urlencoded; charset=UTF-8");
 
     public static ResponseSpecification responseSpecFail200 = new ResponseSpecBuilder()
